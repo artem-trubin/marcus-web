e2fda79611494baa6da6ee84c6d872e374c30b79
artem-trubin <17trubin@gmail.com>
2022-06-03 23:18:01 +0300

    Removing unused data from globals

--- src/globals.js
+++ src/globals.js
@@ -1,18 +1,7 @@
 export const SCREEN_SIZE = { w: 0, h: 0 }
 
-export const keys = {
-    space: { pressed: false },
-    a: { pressed: false },
-    d: { pressed: false },
-}
-
-export const camera = {
-    x: 0,
-    y: 0,
-}
-
-export const solidObjects = []
-
-export const TYPE_SOLID = 'solid'
-export const TYPE_PLAYER = 'player'
-export const TYPE_ACTOR = 'actor'
+export const TYPE_SOLID = "solid"
+export const TYPE_PLAYER = "player"
+export const TYPE_ACTOR = "actor"
+export const TYPE_COIN = "coin"
+export const TYPE_INTERACTIVE = "interactive"
